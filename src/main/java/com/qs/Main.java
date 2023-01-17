85e9c39323ad9029635c5cd8d9e7546c4a3eaecb
LiuChang <374356289@qq.com>
2023-01-17 17:15:31 +0800

    master commit3

--- src/main/java/com/qs/Main.java
+++ src/main/java/com/qs/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Hello world44!");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
